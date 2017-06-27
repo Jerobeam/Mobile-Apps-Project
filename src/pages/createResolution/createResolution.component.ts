fa268351f68c49ad109e62cf8f1891f2af03bced
liasrose <julia.rosenboom@gmx.net>
2017-06-27 11:42:20 +0200

    datenbankübergabe createResolutions

--- src/pages/createResolution/createResolution.component.ts
+++ src/pages/createResolution/createResolution.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {FormBuilder, Validators, FormControl, FormGroup} from '@angular/forms';
+import { Utilities } from '../../app/utilities';
+import {ResolutionProvider} from "../../providers/resolution-provider";
 
 @Component({
   selector: 'page-createResolution',
-  templateUrl: 'createResolution.component.html'
+  templateUrl: 'createResolution.component.html',
+  providers: [ResolutionProvider]
 })
 export class CreateResolutionComponent {
   public resolution ;
+  public isRecurringC: boolean=false;
 
 
-  constructor( public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder ) {
+
+  constructor( public navCtrl: NavController,public utilities: Utilities , public resolutionProvider: ResolutionProvider, public navParams: NavParams, private formBuilder: FormBuilder ) {
     this.resolution = this.formBuilder.group({
       name: [''],
       type: [''],
@@ -18,9 +23,27 @@ export class CreateResolutionComponent {
       imageUrl:['']
     });
   }
-  logForm(){
-    console.log(this.resolution.value)
+
+  isRecurringMethod(){
+    if (this.resolution.value.type == 'Recurring Activity'){
+      this.isRecurringC = true;
+    } if (this.resolution.value.type == 'Single Activity'){
+      this.isRecurringC = false;
+    } else{
+      console.log('jodelahiti');
+    }
+    console.log(this.isRecurringC)
   }
 
-
+  createResolution() {
+    this.isRecurringMethod();
+    console.log(this.resolution.value);
+    this.resolutionProvider.createNewCustomResolution(
+      {
+        isPreconfigured: false,
+        isRecurring: this.isRecurringC,
+        name: this.resolution.value.name,
+        iconUrl: this.resolution.value.imageUrl
+      }, this.utilities.user.uid);
+  }
 }
