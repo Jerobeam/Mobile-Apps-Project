cb47acb37d11eeb1cf235bb46c0f3ab6331467b3
liasrose <julia.rosenboom@gmx.net>
2017-06-27 10:16:24 +0200

    new form create Res

--- src/pages/createResolution/createResolution.component.ts
+++ src/pages/createResolution/createResolution.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {FormBuilder, Validators, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'page-createResolution',
   templateUrl: 'createResolution.component.html'
 })
 export class CreateResolutionComponent {
-  resolution = {};
-  isPreconfigured: boolean =false;
+  private resolution : FormGroup;
+
+  constructor( public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder ) {
+    this.resolution = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: [''],
+    });
+  }
+  logForm(){
+    console.log(this.resolution.value)
+  }
+
+
+  /*isPreconfigured: boolean =false;
   isRecurring: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
@@ -20,7 +33,7 @@ export class CreateResolutionComponent {
 
   setCustomerRes() {
     console.log(this.resolution)
-  }
+  }*/
 
 
 
