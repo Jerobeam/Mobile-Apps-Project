232cd1c136e4d5e88ddb0c3f51c86dece0889db3
Sebastian <se.roehling@gmx.de>
2017-08-01 16:29:46 +0200

    removed comments

--- Einzelarbeiten/Sebastian/MyFramework7App/www/js/my-app.js
+++ Einzelarbeiten/Sebastian/MyFramework7App/www/js/my-app.js
@@ -4,10 +4,6 @@
 // Initialize app and store it to myApp variable for futher access to its methods
 var myApp = new Framework7();
 
-//geofancy
-//push
-//universal links
-
 // We need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 var test = "Test";
